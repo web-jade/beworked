bc2d5f7a1faeca00e26b39f1b7d46480ded9fb9f
Nikita Kostrov <web-jade@yandex.ru>
2018-12-29 00:58:27 +0300

    upload

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1>Hello my app!</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style lang="scss">
+</style>
