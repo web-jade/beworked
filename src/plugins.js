9d2b51f37e9a21cca3a0ba63344673c5b0cca384
Nikita Kostrov <web-jade@yandex.ru>
2018-12-29 01:00:48 +0300

    add vue-bootstrap

--- src/plugins.js
+++ src/plugins.js
@@ -1,1 +1,7 @@
+import Vue from 'vue'
 import 'normalize.css'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
